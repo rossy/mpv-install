dbd4d8ed8f11001f9c3c1b8c1a9871a3f2e9f4ca
James Ross-Gowan <rossymiles@gmail.com>
2015-12-29 22:09:09 +1100

    Use `openfiles` as the administrator test program
    This might be more reliable, since apparently there are situations where
    `net session` doesn't work.
    
    Could help with #1.

--- mpv-install.bat
+++ mpv-install.bat
@@ -190,10 +190,10 @@ exit 0
 	exit 1
 
 :ensure_admin
-	:: 'net session' is just a commmand that is present on all supported
-	:: Windows versions, requires admin privileges and has no side effects, see:
+	:: 'openfiles' is just a commmand that is present on all supported Windows
+	:: versions, requires admin privileges and has no side effects, see:
 	:: https://stackoverflow.com/questions/4051883/batch-script-how-to-check-for-admin-rights
-	net session >nul 2>&1
+	openfiles >nul 2>&1
 	if errorlevel 1 (
 		echo This batch script requires administrator privileges. Right-click on
 		echo mpv-install.bat and select "Run as administrator".
